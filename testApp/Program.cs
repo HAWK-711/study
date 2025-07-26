aad63258ffe05c8299bbbdf85c094399af6e0e27
HAWK_Surface\dev_HAWK <hawk19840720@gmail.com>
2025-07-27 07:34:01 +0900

    テスト用処理を再度追加

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!　この修正はブランチを作成元のものです。");
+Console.WriteLine("Hello, World!　作成元のブランチに処理を追加しました。");
